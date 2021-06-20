b66ecbb39c8490e64f18a7a2460756edb9985ca6
Maria Wright <maria@mariawrightstudios.com>
2021-06-19 20:13:15 -0400

    css for providers desktop

--- src/components/ProviderCard/ProviderCard.js
+++ src/components/ProviderCard/ProviderCard.js
@@ -3,21 +3,25 @@ import './ProviderCard.css';
 
 const ProviderCard = ({ provider }) => (
   <li className="providerCard">
-    <div className="providerCard__image-wrapper">
-      <img
-        className="providerCard__image"
-        src={provider.forProviderImage.url}
-        alt={provider.forProviderTitle}
-      />
-      <h3 className="providerCard__title">{provider.forProviderTitle}</h3>
-      <button
-        type="button"
-        onClick={() => window.open(provider.forProviderButtonUrl)}
-      >
-        {provider.forProviderButtonText}
-      </button>
+    <div
+      className="providerCard__image-wrapper"
+      style={{
+        backgroundImage: `url(${provider.forProviderImage.url})`
+      }}
+    >
+      <div className="providerCard__image-content">
+        <h3 className="card-title_sm">{provider.forProviderTitle}</h3>
+        <button
+          type="button"
+          className="button button_card"
+          onClick={() => window.open(provider.forProviderButtonUrl)}
+        >
+          {provider.forProviderButtonText}
+        </button>
+      </div>
+      <div className="overlay_dark" />
     </div>
-    <p className="providerCard__text">{provider.forProviderDescription}</p>
+    <p className="providerCard__body">{provider.forProviderDescription}</p>
   </li>
 );
 
